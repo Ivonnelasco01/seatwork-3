41a7d9606d0ff396edb25119f8c06fe4e7deded6
Abegail Lasco <ivonnelasco01@gmail.com>
2022-11-20 02:19:14 +0800

    color

--- seatwork 3.py
+++ seatwork 3.py
@@ -3,15 +3,15 @@ import urllib.request as request
 import numpy as np
 import cv2
 #paste ip in variable
-url = "http://192.168.0.102:8080/shot.jpg"
+url = 'http://192.168.0.102:8080/shot.jpg'
 
 while True:
     #open img
     img = request.urlopen(url)
     #convert img to bytes
     img_bytes = bytearray(img.read())
-    img_np = np.array(img_bytes, dtype= np.uint8)
-    frame = cv2.imdecode(img_np,0)
+    img_np = np.array(img_bytes, dtype=np.uint8)
+    frame = cv2.imdecode(img_np, -1)
     #show the img
-    cv2.imshow("Smart Scanner", frame)
+    cv2.imshow('Smart Scanner', frame)
     cv2.waitKey(1)
