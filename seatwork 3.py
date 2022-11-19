dacaa408f052bc9b934485a939229d0a3ae7df07
Abegail Lasco <ivonnelasco01@gmail.com>
2022-11-20 02:08:39 +0800

    import cv2

--- seatwork 3.py
+++ seatwork 3.py
@@ -1,6 +1,7 @@
 #import
 import urllib.request as request
 import numpy as np
+import cv2
 #paste ip in variable
 url = "http://192.168.0.102:8080/shot.jpg"
 
@@ -10,3 +11,4 @@ while True:
     #convert img to bytes
     img_bytes = bytearray(img.read())
     img_np = np.array(img_bytes, dtype= np.uint8)
+    frame = cv2.imdecode(img_np,0)
