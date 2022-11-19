c840b79e24419ddb8ac32b11a7b4f00e02109e23
Abegail Lasco <ivonnelasco01@gmail.com>
2022-11-20 02:07:36 +0800

    import numpy

--- seatwork 3.py
+++ seatwork 3.py
@@ -1,5 +1,6 @@
 #import
 import urllib.request as request
+import numpy as np
 #paste ip in variable
 url = "http://192.168.0.102:8080/shot.jpg"
 
@@ -8,3 +9,4 @@ while True:
     img = request.urlopen(url)
     #convert img to bytes
     img_bytes = bytearray(img.read())
+    img_np = np.array(img_bytes, dtype= np.uint8)
